9e9723ad17b0fcf301946f806b266f9cff59c1eb
Konrad Grüner <konraifen88@googlemail.com>
2016-11-17 15:33:24 +0100

    change extra proxyconfig to output some help

--- init/conf_proxy_extra.sh
+++ init/conf_proxy_extra.sh
@@ -1,14 +1,14 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash 
 
 # see http://stackoverflow.com/questions/31205438/docker-on-windows-boot2docker-certificate-signed-by-unknown-authority-error
-
-CERT_DIR = /vagrant/init/certificates/
+CERT_DIR="/vagrant/init/certificates"
+DOCKER_KEY="/vagrant/init/gpgkey.asc"
 
 if [ "$(ls -A $CERT_DIR)" ]; then
 	echo "### install certificates ###"
     mkdir -p /etc/docker/certs.d/
-	cp /vagrant/init/certificates/* /etc/docker/certs.d/
-	cp /vagrant/init/certificates/* /usr/local/share/ca-certificates/
+	cp $CERT_DIR/* /etc/docker/certs.d/
+	cp $CERT_DIR/* /usr/local/share/ca-certificates/
 	update-ca-certificates
 else
     echo "No Certificates found!"
@@ -16,6 +16,15 @@ else
 fi
 
 
-echo "### install keyfile ###"
+
 # Downloaded key from http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xF76221572C52609D directly
-apt-key add /vagrant/init/docker.key
+if [ -f $DOCKER_KEY ]; then
+	echo "### install keyfile ###"
+	apt-key add $DOCKER_KEY
+else
+	echo "No key-file was found."
+	echo "You can download it from:"
+	echo "http://pgp.mit.edu/pks/lookup?op=get&options=mr&search=0x58118E89F3A912897C070ADBF76221572C52609D"
+	echo "if the following commands will fail."
+	echo "Simply download it and place it to: <project_dir>/init/gpgkey.asc"
+fi
